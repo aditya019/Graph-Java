699fa5bd955c535f247ef32dedde9b62ee1897c9
Aditya <55052481+aditya019@users.noreply.github.com>
2022-02-19 00:46:56 +0530

    Delete src directory

--- src/Edge.java
+++
@@ -1,9 +0,0 @@
-public class Edge {
-    int source, destination, weight;
-
-    public Edge(int source, int destination, int weight) {
-        this.source = source;
-        this.destination = destination;
-        this.weight = weight;
-    }
-}
